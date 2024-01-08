c268c21ff8ea6c1496bd5f7a51defcd85829c5a8
Markus Gavra <145468935+GavraMG@users.noreply.github.com>
2024-01-08 00:15:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,208 @@
+// Skill Toggle
+function elemToggleFunc(element) {
+  element.classList.toggle('active');
+}
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+toggleBtns[i].addEventListener("click", function () {
+
+  elemToggleFunc(toggleBtnBox);
+  for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+  elemToggleFunc(skillsBox);
+
+});
+}
+
+// Social Card
+const showSocial = (toggleCard, socialCard) =>{
+  const toggle = document.getElementById(toggleCard),
+        social = document.getElementById(socialCard)
+  
+  toggle.addEventListener('click', ()=>{
+      // If the animation class exists, we add the down-animation class
+      if(social.classList.contains('animation')){
+          social.classList.add('down-animation')
+
+          // We remove the down-animation class
+          setTimeout(() =>{
+              social.classList.remove('down-animation')
+          }, 1000)
+      }
+
+      // We add the animation class to the div tag with the card__social class
+      social.classList.toggle('animation')
+  })
+}
+showSocial('card-toggle','card-social')
+
+// -------------------------------------------------------------------------------------
+
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.scrollY;
+
+  // Homepage transition
+  var maxScroll;
+  if (window.innerWidth > 500) {
+    maxScroll = 160; // adjust this value as needed
+  } else {
+    maxScroll = 200;
+  }
+
+  var homepageOpacity = 1 - scrollPosition / maxScroll;
+  var nextSectionOpacity = 0;
+
+  if (homepageOpacity < 0) {
+    homepageOpacity = 0;
+    nextSectionOpacity = (scrollPosition - maxScroll) / maxScroll;
+  }
+
+  if (nextSectionOpacity > 1) nextSectionOpacity = 1;
+
+  document.getElementById("homepage").style.opacity = homepageOpacity;
+  document.getElementById("nextSection").style.opacity = nextSectionOpacity;
+
+  // Sticky navbar effect
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// -------------------------------------------------------------------------------------
+
+// Home Page Particle Background Effect:
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ff5f1f",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+
+// About Square Background Particle Effect:
+particlesJS("particles-js-halfSquare", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#FFFFFF",
+      opacity: 0.4,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: false, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+
+var count_particles, stats, update;
+stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function () {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
+
+// -------------------------------------------------------------------------------------
+
